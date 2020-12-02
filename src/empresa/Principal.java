c4636ffdbbbac500eda10247139dc368a1c27233
3520 <3520@RAFAEL>
2020-12-01 21:34:22 -0300

    ADD: Prueba final Java Mod2

---
+++ src/empresa/Principal.java
@@ -0,0 +1,29 @@
+/*Carol Llanca
+Jaime Donoso
+Ignacio Caro
+Rafael Calderón*/
+
+package empresa;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+
+		Scanner sc = new Scanner(System.in);
+
+		Menu menu = new Menu();
+
+		int opcion = 0;
+		do { 
+			opcion = menu.menu();
+		}
+		while ( opcion != 9);
+
+		sc.close();
+
+	}
+
+}
